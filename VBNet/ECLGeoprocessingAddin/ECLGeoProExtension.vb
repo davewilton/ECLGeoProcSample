ae1e7005b1c168c1c857ca341a0611856fed0a98
David Wilton <djwilton@gmail.com>
2013-11-26 09:52:09 +0000

    renamed project

--- VBNet/ECLGeoprocessingAddin/ECLGeoProExtension.vb
+++ VBNet/ECLGeoprocessingAddin/ECLGeoProExtension.vb
@@ -1,6 +1,4 @@
-﻿Imports ECLUtility
-
-Public Class ECLGeoProExtension
+﻿Public Class ECLGeoProExtension
     Inherits ESRI.ArcGIS.Desktop.AddIns.Extension
 
     Public Sub New()
@@ -8,9 +6,6 @@ Public Class ECLGeoProExtension
     End Sub
 
     Protected Overrides Sub OnStartup()
-        '
-        ' TODO: Uncomment to start listening to document events
-        'WireDocumentEvents()
 
         'Create the toolbox. WARNING this is slow, don't call on every start up in production
         CreateToolbox()
@@ -21,15 +16,4 @@ Public Class ECLGeoProExtension
 
     End Sub
 
-    Private Sub WireDocumentEvents()
-        '
-        ' TODO: Sample document event wiring code. Change as needed.
-        '
-        AddHandler My.ArcMap.Events.NewDocument, AddressOf ArcMapNewDocument
-
-    End Sub
-
-    Private Sub ArcMapNewDocument()
-
-    End Sub
 End Class
